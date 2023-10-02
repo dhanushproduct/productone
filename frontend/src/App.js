f1026dab9e60a1b3fc11e0ac214932d508114654
dhanush <dhanushg25@gmail.com>
2023-10-02 23:20:49 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-slate-400 h-screen w-full flex  justify-center items-center">
+     
+     <p className='h-fit text-2xl uppercase'>
+    hello world
+     </p>
+    </div>
+  );
+}
+
+export default App;
