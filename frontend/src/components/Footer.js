cbef1ee683acf14d27db767dc3babf229e70d32d
dhanush <dhanushg25@gmail.com>
2023-10-05 19:23:37 +0530

    home page framed

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-full h-[70vh]'>
+        <div className='flex'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        {/* social media handles */}
+        <div></div>
+        <hr />
+        {/* copy rights */}
+        <div></div>
+    </div>
+  )
+}
